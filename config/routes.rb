fb05743dd5e35ddd88827a75205c7f394cbef548
gene-yoo <gene.y.yoo@gmail.com>
2017-10-24 10:50:26 -0400

    Lab complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :venues
-  resources :events
-  resources :groups
-  resources :users
+  get '/', to: 'application#hello', as: 'root'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+
+  get '/signup', to: 'users#new', as: 'signup'
+
+  resources :users, only: [:show]
+  resources :events, only: [:create, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
