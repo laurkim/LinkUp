2ee5e439918221b619b59685fca754eaa274e8c7
laurkim <laurakeem@gmail.com>
2017-10-24 13:12:00 -0400

    reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,6 @@ ActiveRecord::Schema.define(version: 20171024135618) do
     t.datetime "end_time"
     t.string "image_url"
     t.boolean "free"
-    t.integer "user_id"
-    t.integer "group_id"
     t.integer "venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -40,8 +38,6 @@ ActiveRecord::Schema.define(version: 20171024135618) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
-    t.integer "event_id"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -54,16 +50,16 @@ ActiveRecord::Schema.define(version: 20171024135618) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "venues", force: :cascade do |t|
-    t.string "name"
-    t.float "latitude"
-    t.float "longitude"
+    t.string "address"
+    t.string "venue_no"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
