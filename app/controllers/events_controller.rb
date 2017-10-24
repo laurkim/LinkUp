6807c7ffc430995a145544a188ed7a1518739827
laurkim <laurakeem@gmail.com>
2017-10-24 06:42:46 -0400

    added views and controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,34 @@
 class EventsController < ApplicationController
+
+  before_action :set_event, only: [:show]
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+    def set_event
+      @event = Event.find_by(id: params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit()
+    end
+
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,18 @@
 class VenuesController < ApplicationController
+
+  before_action :set_venue, only: [:show]
+
+  def show
+  end
+
+  private
+
+    def set_venue
+      @venue = Venue.find_by(id: params[:id])
+    end
+
+    def venue_params
+      params.require(:venue).permit()
+    end
+
 end
