175e92c44660087ad56f57fae6f919a874b6f3e0
gene-yoo <gene.y.yoo@gmail.com>
2017-10-27 13:57:09 -0400

    Add login verificaiton for Event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :check_if_logged_in, only [:show]
+  before_action :check_if_logged_in, only: [:show]
   before_action :set_event, only: [:show]
 
   def index
